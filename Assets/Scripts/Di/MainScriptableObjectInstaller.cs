a15756ddf72da76e6ec811cc5acfb51945353a77
Petrushevski Apps <petrushevskiapps@gmail.com>
2026-01-21 22:44:15 +0100

    Added and setup Zenject plugin integration

---
+++ Assets/Scripts/Di/MainScriptableObjectInstaller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Utilities
+{
+    [CreateAssetMenu(
+        fileName = "ScriptableObjectInstaller",
+        menuName = "Configuration/ScriptableObjectInstaller")]
+    public class MainScriptableObjectInstaller : ScriptableObjectInstaller<MainScriptableObjectInstaller>
+    {
+        [SerializeField]
+        private List<ScriptableObject> _scriptableObjectsToBind;
+
+        public override void InstallBindings()
+        {
+            foreach (var scriptableObject in _scriptableObjectsToBind)
+            {
+                Container
+                    .BindInterfacesTo(scriptableObject.GetType())
+                    .FromInstance(scriptableObject);
+                Container.QueueForInject(scriptableObject);
+            }
+        }
+    }
+}
