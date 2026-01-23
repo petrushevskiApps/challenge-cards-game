ac3f07f7433751e3c82672b9704817c92c28f227
Petrushevski Apps <petrushevskiapps@gmail.com>
2026-01-23 13:52:33 +0100

    Implemented Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using TwoOneTwoGames.UIManager.ScreenNavigation;
+using UnityEngine;
+using Zenject;
+
+namespace DefaultNamespace
+{
+    public class GameManager: MonoBehaviour, IPauseGameController
+    {
+        private const int UNLIMITED_FPS = -1;
+        private const int MOBILE_FPS = 120;
+        
+        public event Action<bool> GamePausedEvent;
+        
+        private bool _isGamePaused;
+
+        [Inject]
+        private IScreenNavigation _screenNavigation;
+        
+        private void Awake()
+        {
+            SetupGame();
+        }
+        
+        private void SetupGame()
+        {
+            Input.multiTouchEnabled = false;
+            SetupFrameRate();
+            _screenNavigation.ShowMainScreen();
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+
+        public void TogglePauseGame(bool pause)
+        {
+            if (pause)
+            {
+                Time.timeScale = 0;
+                _isGamePaused = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                _isGamePaused = false;
+            }
+
+            GamePausedEvent?.Invoke(_isGamePaused);
+        }
+
+        private void SetupFrameRate()
+        {
+            Application.targetFrameRate = Application.isEditor ? UNLIMITED_FPS : MOBILE_FPS;
+        }
+    }
+}
